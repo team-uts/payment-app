a61d905d4e520e8e45679f9c3fa24579e99ee674
joshua-hb-lee <lhb1025@gmail.com>
2026-08-18 22:20:46 +1000

    feat: test configuration

---
+++ buildSrc/src/main/kotlin/task/BuildLifecyclePlugin.kt
@@ -0,0 +1,29 @@
+package task
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.internal.build.event.BuildEventListenerRegistryInternal
+import org.gradle.invocation.DefaultGradle
+
+class BuildLifecyclePlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        val gradle = project.gradle
+
+        val services = (gradle as DefaultGradle).services
+        val registry = services[BuildEventListenerRegistryInternal::class.java]
+
+        val operationService = gradle.sharedServices.registerIfAbsent(
+            "operationService",
+            BuildOperationService::class.java
+        ) {
+            gradle.taskGraph.whenReady {
+                parameters.targetTaskPaths =
+                    this.allTasks
+                        .map { it.path }
+                        .filter { it.endsWith(":test") }
+            }
+        }
+
+        registry.subscribe(operationService)
+    }
+}
---
+++ buildSrc/src/main/kotlin/task/BuildOperationService.kt
@@ -0,0 +1,26 @@
+package task
+
+import org.gradle.api.services.BuildService
+import org.gradle.api.services.BuildServiceParameters
+import org.gradle.tooling.events.FinishEvent
+import org.gradle.tooling.events.OperationCompletionListener
+import org.gradle.tooling.events.task.TaskFinishEvent
+import task.test.TestContainer
+import task.test.TestLoggingUtils
+
+abstract class BuildOperationService : BuildService<BuildOperationService.Params>, OperationCompletionListener {
+    interface Params : BuildServiceParameters {
+        var targetTaskPaths: List<String>
+    }
+
+    override fun onFinish(event: FinishEvent?) {
+        if (event == null || event !is TaskFinishEvent) {
+            return
+        }
+
+        if (parameters.targetTaskPaths.contains(event.descriptor.taskPath)) {
+            TestLoggingUtils.printTotalResult(TestContainer.testResults)
+            TestContainer.testResults = null
+        }
+    }
+}
