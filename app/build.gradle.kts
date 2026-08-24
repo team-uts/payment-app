402ca59bfe53b4e7a0339a2d09f52a85048bec0c
joshua-hb-lee <lhb1025@gmail.com>
2026-08-24 23:31:22 +1000

    feat: test configuration 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,19 @@
+afterEvaluate {
+    project.tasks.apply {
+        // depends on sub-modules test tasks
+        this.withType<Test> {
+            dependsOn(
+                listOf(
+                    ":domain",
+                    ":shared",
+                    ":persistence",
+                    ":infra"
+                ).map { "$it:test" }
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(project(":domain"))
     implementation(project(":shared"))
