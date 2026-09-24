582dd79730fb0caedebcd43b6db691e51cc88984
joshua-hb-lee <lhb1025@gmail.com>
2026-09-24 17:47:14 +1000

    [#3] feat: payment list api developing

--- app/src/main/java/dev/teamuts/payment/app/api/PaymentMethodApiController.java
+++ app/src/main/java/dev/teamuts/payment/app/api/PaymentMethodApiController.java
@@ -10,6 +10,7 @@ import dev.teamuts.payment.domain.paymentmethod.usecase.SetupPaymentMethodRegist
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,11 @@ public class PaymentMethodApiController {
     SetupPaymentMethodInfo result = setupPaymentMethodUseCase.execute(command);
 
     return ApiResponse.success(result);
+  }
+
+  @GetMapping
+  public ApiResponse<String> getPaymentMethodList() {
+    // TODO : implement getPaymentMethodList
+    return ApiResponse.success("ok");
   }
 }
